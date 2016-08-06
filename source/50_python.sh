3d1881a8d2db6acf6eeb4b7e42a5a14f62acc41c
Mike Lockhart <sinewalker@gmail.com>
2016-08-07 08:01:21 +1000

    virtualenv-wrapper work-alikes
    Kind of re-inventing the wheel, but these work on Python 2 OR 3. Also it
    keeps all of my virtual env's together in one place, where I want them.

--- source/50_python.sh
+++ source/50_python.sh
@@ -7,17 +7,70 @@ gpip() {
 	PIP_REQUIRE_VIRTUALENV="" pip "$@"
 }
 
-####
+#### virtualenv-wrapper work-alikes
 
+#this is where Python Virtual Environments belong
 export VIRTUALENV_BASE=${HOME}/lib
 
-workon() {
-	if [ -d ${VIRTUALENV_BASE}/${1} ]; then
-		source ${VIRTUALENV_BASE}/${1}/bin/activate
-	else
-		echo "No such env: ${1}"
-	fi 
+error() {
+    >&2 echo $@
 }
 
-alias activate=workon
+usage() {
+    USAGE=${1}; shift
+    MESSAGE=$@
+    error Usage: ${USAGE}
+    echo ${MESSAGE}
+}
 
+mkvenv() {
+    FUNCDESC="Makes a Python Virtual env in ${VIRTUALENV_BASE}."
+    [ -z ${1} ] && usage "$FUNCNAME <venv> [virtualenv options]" $FUNCDESC && return 1
+    VENV=${VIRTUALENV_BASE}/${1}
+    [ -d ${VENV} ] && error "$FUNCNAME: Directory exists: ${VENV}" && return 1
+    shift
+    virtualenv $@ ${VENV}
+}
+
+rmvenv() {
+    FUNCDESC="Interactively removes a Python Virtual env from ${VIRTUALENV_BASE}"
+    [ -z ${1} ] && usage "$FUNCNAME <venv>" $FUNCDESC && return 1
+    VENV=${VIRTUALENV_BASE}/${1}
+    if [ -f ${VENV}/bin/activate ]; then
+        read -r -p "Remove Venv: ${1}? [y/N] " REMOVE
+        REMOVE=${REMOVE,,} #to-lower
+        if [[ ${REMOVE} =~ ^(yes|y)$ ]]; then
+            rm -rf ${VENV}
+        else
+            echo "$FUNCNAME: aborted"
+        fi
+    else
+        error "$FUNCNAME: No such Venv: ${1}"
+        return 1
+    fi
+}
+
+lsvenv() {
+    FUNCDESC="Lists Python Virtual envs installed to ${VIRTUALENV_BASE}"
+    for FILESPEC in ${VIRTUALENV_BASE}/*/bin/activate; do
+        basename $(dirname $(dirname ${FILESPEC}))
+    done
+}
+
+activate() {
+    read -d '' FUNCDESC <<EOV
+Activtes a Python Virtual env that's in ${VIRTUALENV_BASE}
+(will deactivate current Venv if one is active).
+EOV
+    [ -z ${1} ] && usage "$FUNCNAME <venv>" $FUNCDESC && return 1
+    VENV=${VIRTUALENV_BASE}/${1}
+    if [ -f ${VENV}/bin/activate ]; then
+        type deactivate &> /dev/null && deactivate
+        source ${VENV}/bin/activate
+    else
+        error "$FUNCNAME: No such Venv: ${1}"
+        echo "Available Python Venvs are:"
+        lsvenv
+        return 1
+    fi
+}
