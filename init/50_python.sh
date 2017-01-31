ebfb60166a7fa3f100c33dff3162c048bf79aa38
Mike Lockhart <sinewalker@gmail.com>
2017-02-01 06:35:50 +1100

    note bug in Linux for future

--- init/50_python.sh
+++ init/50_python.sh
@@ -5,6 +5,8 @@ source $DOTFILES/source/50_python.sh
 # see http://hackercodex.com/guide/python-development-environment-on-mac-osx/
 # (which ideas are more widely applicable than just macOS)
 
+#TODO BUG: permissions error on Linux (because it tries to install to global locations)
+#          it may be better to do global installs using the OS package manager
 gpip install --upgrade pip setuptools wheel virtualenv
 gpip install --upgrade Mercurial hg-git
 gpip install --upgrade isort ipython
