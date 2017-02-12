4ec684e7d9f735d08eb5b447a797fe48cbe3c687
Mike Lockhart <sinewalker@gmail.com>
2017-02-12 22:35:12 +1100

    remove Cowboy's ssh|scp|sftp-specific _known_hosts completion
    plus, defensive programming FTW: do check bash_completions exists before
    sourcing it....

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -15,15 +15,13 @@ function titlebar() {
   echo -n $'\e]0;'"$*"$'\a'
 }
 
-# SSH auto-completion based on entries in known_hosts.
-#if [[ -e ~/.ssh/known_hosts ]]; then
-#  complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp sftp
-#fi
-#MJL20170212 proper bash_completion using bash_completion package
-# System-wide completions
+#MJL20170212 bash_completion using bash_completion package
+
+# source System-wide completions (if installed)
 local BC=/etc/bash_completion
 is_osx && BC=/usr/local/etc/bash_completion
-source ${BC}
+[[ -f ${BC} ]] && source ${BC}
+
 # Personal completions (if any)
 is_osx && XDG_CONFIG_HOME=${HOME}/.config
 [[ -f ${XDG_CONFIG_HOME}/bash_completion ]] && source ${XDG_CONFIG_HOME}/bash_completion
