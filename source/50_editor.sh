336d260afaf6ffe3a6e790831a4c1135cd84ffca
Mike Lockhart <sinewalker@gmail.com>
2017-02-14 22:48:38 +1100

    fold ASharpe's LESS/PAGER settings into 50_editor.sh

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -18,3 +18,16 @@ alias mce=mcedit
 alias mcv=mcview
 alias mcd=mcdiff
 alias view=mcview
+
+# configure PAGER/LESS
+# stolen from http://merlinmoncure.blogspot.com.au/2007/10/better-psql-with-less.html
+export PAGER=less
+
+# -i ignore case in search
+# -M long prompt ?
+# -S chop long lines
+# -x4 tab stop at multiples of 4
+# -F quit if one screen
+# -X no (de)initialisation (avoids clearing screen etc)
+# -R handle colours nicely
+export LESS="-iMSx4 -FXR"
