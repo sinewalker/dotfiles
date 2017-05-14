30909f1bd48b205520a3c16986c9ba524efbff23
Mike Lockhart <sinewalker@gmail.com>
2017-05-14 10:01:16 +1000

    hammerspoon - new modular init

--- link/.hammerspoon/init.lua
+++ link/.hammerspoon/init.lua
@@ -1,9 +1,8 @@
-hs.hotkey.bind({"cmd","alt"}, "f12", function() hs.application.launchOrFocus("iTerm") end)
-hs.hotkey.bind({"cmd","alt"}, "f11", function() hs.application.launchOrFocus("Emacs") end)
-hs.hotkey.bind({"cmd","alt"}, "f10", function() hs.application.launchOrFocus("HipChat 2") end)
-hs.hotkey.bind({"cmd","alt"}, "f9", function() hs.application.launchOrFocus("KeePassX") end)  -- or "Enpass"
+-- settings first
+--hs.window.setFrameCorrectness = true
+--hs.window.animationDuration = 0
 
-hs.hotkey.bind({"cmd","alt"}, "f8", function() hs.application.launchOrFocus("Linphone") end)
-
-hs.hotkey.bind({"cmd","alt"}, "f6", function() hs.application.launchOrFocus("Spotify") end)
-hs.hotkey.bind({"cmd","alt"}, "f5", function() hs.application.launchOrFocus("Google Chrome") end)
+-- start the magic
+require "util"
+require "jumpcut"
+require "keys"
