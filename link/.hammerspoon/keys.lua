6b774a72c23d15c96fcf207a4c118242634eff91
Mike Lockhart <sinewalker@gmail.com>
2017-05-14 10:00:00 +1000

    hammerspoon - pasteboard, jump cut and shortcuts

---
+++ link/.hammerspoon/keys.lua
@@ -0,0 +1,16 @@
+omega = {"cmd", "alt", "ctrl"}
+hyper = {"cmd", "alt"}
+
+hs.hotkey.bind(omega, 'r', hs.reload)
+hs.hotkey.bind(omega, 'f', maximise)
+hs.hotkey.bind(omega, 'v', showClipboard)
+
+hs.hotkey.bind(hyper, "f12", function() hs.application.launchOrFocus("iTerm") end)
+hs.hotkey.bind(hyper, "f11", function() hs.application.launchOrFocus("Emacs") end)
+hs.hotkey.bind(hyper, "f10", function() hs.application.launchOrFocus("HipChat 2") end)
+hs.hotkey.bind(hyper, "f9", function() hs.application.launchOrFocus("KeePassX") end)  -- or "Enpass"
+
+hs.hotkey.bind(hyper, "f8", function() hs.application.launchOrFocus("Linphone") end)
+
+hs.hotkey.bind(hyper, "f6", function() hs.application.launchOrFocus("Clementine") end) -- or "Spotify"
+hs.hotkey.bind(hyper, "f5", function() hs.application.launchOrFocus("Google Chrome") end)
