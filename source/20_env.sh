a9cb293ca3bdfa7d52ec69823db4bebdf543458d
Mike Lockhart <sinewalker@gmail.com>
2017-10-14 10:43:10 +1100

    move shell options and grep environment into source/20_env.sh
    These aren't "miscellaneous" -- they're environment settings

--- source/20_env.sh
+++ source/20_env.sh
@@ -1,3 +1,12 @@
+# Case-insensitive globbing (used in pathname expansion)
+shopt -s nocaseglob
+
+# Check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+export GREP_OPTIONS='--color=auto'
+
 #show where a command comes from
 alias whence='type -a'
 complete -F _command whence
--- source/50_misc.sh
+++ source/50_misc.sh
@@ -1,12 +1,3 @@
-# Case-insensitive globbing (used in pathname expansion)
-shopt -s nocaseglob
-
-# Check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
-shopt -s checkwinsize
-
-export GREP_OPTIONS='--color=auto'
-
 # Set the terminal's title bar.
 function titlebar() {
   echo -n $'\e]0;'"$*"$'\a'
