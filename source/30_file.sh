7dae109a6d20b046ce10d5d8008ec661a4834a01
Mike Lockhart <sinewalker@gmail.com>
2018-03-16 22:11:52 +1100

    link the z manpage if required

--- source/30_file.sh
+++ source/30_file.sh
@@ -88,13 +88,13 @@ source ${DOTFILES}/vendor/bashmarks/bashmarks.sh
 
 # Fast directory switching
 
-# TODO: the manual page for z needs to be linked or copied somewhere that will
-# be picked up by the man command, e.g:
-#
-# ln -fs ${DOTFILES}/vendor/z/z.1 /usr/local/share/man/man1/
-#
-# but doing that every time this file is sourced would be silly.
-# (and setting $MANPATH overrides /etc/man.conf)
+# Link the manual into the local manpath, if needed and present.
+local zman zmanlink
+zman=${DOTFILES}/vendor/z/z.1
+zmanlink=/usr/local/share/man/man1/
+if [[ ! -L ${zmanlink}/z.1 ]] && [[ -e ${zman} ]]; then
+    ln -fs ${zman} ${zmanlink}
+fi
 
 mkdir -p ${DOTFILES}/caches/z
 #_Z_NO_PROMPT_COMMAND=1
