6512261195e8c327bc32a631e6dc15d073e54d73
Mike Lockhart <sinewalker@gmail.com>
2018-04-19 23:34:19 +1000

    initial hack at a suse-zypper package installer
    based loosely upon the upbuntu example.
    
    This isn't working -- the setdiff is not cancelling out the already installed
    packages like it should have. I need to work out what is wrong with my rpm|awk
    pipeline.
    
    Also some of the packages I took from hax-vm are different/gone in Tumbleweed
    
    But it's basically correct appart from these!

---
+++ init/20_suse_zypper.sh
@@ -0,0 +1,65 @@
+# SUSE-only stuff. Abort if not SUSE.
+is_suse || return 1
+
+
+# Update Zypper.
+e_header "Updating Zypper"
+cat <<EOM
+You will need to enter the root password for installing packages.
+
+If you don't know the root password, just press Enter to skip these.
+
+EOM
+
+e_header "Refreshing Zypper repos"
+sudo zypper --quiet refresh
+
+#e_header "Upgrading installed packages"
+#sudo zypper --quiet update -y
+
+e_header "Installing SUSE Patterns"
+sudo zypper install -y \
+     pattern:console pattern:devel_basis pattern_python
+
+# Install RPM packages.
+PACKAGES=(
+    ansible
+    cowsay
+    xcowsay
+    htop
+    nano
+    screen
+    tmux
+    libopenssl-devel
+    password-store
+    python3-virtualenv
+    python3-virtualenv-doc
+    python-devel
+    pychecker
+    python3-pip
+    git-core
+    mercurial
+    nmap
+    telnet
+    tree
+    nodejs-common
+)
+
+PACKAGES=($(setdiff "${PACKAGES[*]}" "$(rpm -qa |awk --field-separator=-[0-9] '{print $1}')"))
+
+if (( ${#PACKAGES[@]} > 0 )); then
+  e_header "Installing RPM packages: ${PACKAGES[*]}"
+  for PACKAGE in "${PACKAGES[@]}"; do
+    echo "🡒 ${PACKAGE}"
+    sudo zypper install -y "${PACKAGE}"
+  done
+fi
+
+# Install Git Extras
+if [[ ! "$(type -P git-extras)" ]] && [[ -d ${DOTFILES}/vendor/git-extras ]]; then
+  e_header "Installing Git Extras"
+  (
+    cd $DOTFILES/vendor/git-extras &&
+    sudo make install
+  )
+fi
