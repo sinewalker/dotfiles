c48eecaf309300e84d7cb7b29f9f71c62ecaa907
Mike Lockhart <sinewalker@gmail.com>
2019-06-01 09:35:22 +1000

    hot key assignments
     - Use Visual Studio Code as go-to editor instead of Emacs
     - Work switched to Slack from Hipchat
     - Don't use keepassx any more

--- link/.hammerspoon/keys.lua
+++ link/.hammerspoon/keys.lua
@@ -6,9 +6,8 @@ hs.hotkey.bind(omega, 'f', maximise)
 hs.hotkey.bind(omega, 'v', showClipboard)
 
 hs.hotkey.bind(hyper, "f12", function() hs.application.launchOrFocus("iTerm") end)
-hs.hotkey.bind(hyper, "f11", function() hs.application.launchOrFocus("Emacs") end)
-hs.hotkey.bind(hyper, "f10", function() hs.application.launchOrFocus("HipChat 2") end)
-hs.hotkey.bind(hyper, "f9", function() hs.application.launchOrFocus("KeePassX") end)  -- or "Enpass"
+hs.hotkey.bind(hyper, "f11", function() hs.application.launchOrFocus("Visual Studio Code") end)
+hs.hotkey.bind(hyper, "f10", function() hs.application.launchOrFocus("Slack") end)
 --hs.hotkey.bind(hyper, "f9", function() hs.application.launchOrFocus("KeePassX") end)  -- or "Enpass"
 
 hs.hotkey.bind(hyper, "f9", function() hs.application.launchOrFocus("Linphone") end)
