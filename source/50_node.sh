a5f09ad09c07a0f5c5bd502b2a556482c6d0ccae
Mike Lockhart <sinewalker@gmail.com>
2019-07-17 13:17:47 +1000

    DRY nvm function loading
    add $NODE_DIR variable
    separate NVM_DIR and NODE_DIR path loading

--- source/50_node.sh
+++ source/50_node.sh
@@ -1,9 +1,15 @@
 #TODO - work out if there's more to do here. Probably there is a lot of npm
 #stuff I could set up
 
+# Node Version Manager and it's managed Nodes go here
 export NVM_DIR="${LIBRARY}/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-path_add ${LIBRARY}/nvm
-path_add ${LIBRARY}/node/bin
+# Load nvm functions and bash completion
+for NVM_MODULE in ${NVM_DIR}/nvm.sh ${NVM_DIR}/bash_completion; do
+    load "${NVM_MODULE}"
+done
+
+path_add ${NVM_DIR}
+
+export NODE_DIR=${LIBRARY}/node
+path_add ${NODE_DIR}/bin
