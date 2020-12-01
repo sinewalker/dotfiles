7ac549da43fd382d6a36936d1c2b9fc56f05aa81
Mike Lockhart <mlockhart@gitlab.com>
2020-12-01 12:45:44 +1100

    remove vagrant stuff from asharpe

--- source/50_vagrant.sh
+++ source/50_vagrant.sh
@@ -7,10 +7,3 @@ export VAGRANT_HOME=~/lib/vagrant
 # These get quite huge, and I want them elsewhere than Dotfiles, or my Libs
 export VAGRANT_BOXES=~/vms/boxes
 
-# Set the terminal's title bar.
-function titlebar() {
-  echo -n $'\e]0;'"$*"$'\a'
-}
-
-# Disable ansible cows }:]
-export ANSIBLE_NOCOWS=1
