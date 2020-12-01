fd296d3760001d32609a28bf5ffd08b6284e14c1
Mike Lockhart <mlockhart@gitlab.com>
2020-12-01 12:45:07 +1100

    rbenv setup

--- init/50_ruby.sh
+++ init/50_ruby.sh
@@ -3,9 +3,60 @@
 source ${DOTFILES}/source/10_meta.sh
 source ${DOTFILES}/source/50_ruby.sh
 
-
-# https://about.gitlab.com/handbook/git-page-update/  with some tweeks
+[[ -z ${RBENV_ROOT} ]] && exit 1
 
 # Install RVM -- Note that source/50_ruby.sh sets RVM_DIR to the right place
-curl -sSL https://get.rvm.io | bash -s stable
+#curl -sSL https://get.rvm.io | bash -s stable
 
+# While https://about.gitlab.com/handbook/git-page-update/  recommended RVM,
+# https://gitlab.com/gitlab-com/www-gitlab-com/-/blob/master/doc/development.md and
+# https://gitlab.com/gitlab-org/gitlab-development-kit/-/blob/master/doc/prepare.md
+# recommend rbenv instead.
+
+# https://github.com/rbenv/rbenv-installer/blob/master/bin/rbenv-installer is 
+# a good starting point, but it has a hard-coded install location, and it does
+# too much :-\
+
+if is_osx && is_exe brew;  then
+    if is_exe rbenv; then
+      echo "Updating rbenv and ruby-build with Homebrew..."
+      brew update
+      brew upgrade rbenv ruby-build
+    else
+      echo "Installing rbenv and ruby-build with Homebrew..."
+      brew install rbenv ruby-build
+    fi
+else
+    #TODO test this and make it re-runnable (or just ansiblize ffs)
+    echo "Installing rbenv with git..."
+    mkdir -p ${RBENV_ROOT}
+    cd ${RBENV_ROOT}
+    git init
+    git remote add -f -t master origin https://github.com/rbenv/rbenv.git
+    git checkout -b master origin/master
+
+    echo "Installing ruby-build with git..."
+    mkdir -p "${RBENV_ROOT}/plugins"
+    git clone https://github.com/rbenv/ruby-build.git "${RBENV_ROOT}/plugins/ruby-build"
+fi
+
+# Enable caching of rbenv-install downloads
+mkdir -p "${RBENV_ROOT}/cache"
+
+echo "Running doctor script to verify installation..."
+eval "$(rbenv init -)"
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
+
+#https://github.com/tpope/rbenv-communal-gems
+communal_gems=${RBENV_ROOT}/plugins/rbenv-communal-gems
+
+echo -n Communal gems...
+if [[ -d ${communal_gems}  ]] ; then
+  cd ${communal_gems}
+  git pull
+else
+  mkdir -p ${RBENV_ROOT}/plugins
+  git clone git://github.com/tpope/rbenv-communal-gems.git \
+    ${RBENV_ROOT}/plugins/rbenv-communal-gems
+fi
+rbenv communize --all
--- source/50_ruby.sh
+++ source/50_ruby.sh
@@ -4,14 +4,29 @@
 export RUBY_DIR=${LIB}/ruby
 [[ -d ${RUBY_DIR} ]] || mkdir -p ${RUBY_DIR}
 
+## RVM
 # Ruby Version Manager and it's managed Rubys go here, thanks
-export RVM_DIR="${LIB}/ruby/rvm"
+#export RVM_DIR="${LIB}/ruby/rvm"
 
 # Load RVM into a shell session *as a function*
-[[ -s "${RVM_DIR}/scripts/rvm" ]] && source "${RVM_DIR}/scripts/rvm" 
+#[[ -s "${RVM_DIR}/scripts/rvm" ]] && source "${RVM_DIR}/scripts/rvm" 
 
 # Add RVM to PATH for scripting.
-path_add ${RVM_DIR}/bin
+#path_add ${RVM_DIR}/bin
 
 # rbenv is incompatible with RVM, but this is how to init it anyway
-#is_exe rbenv && eval "$(rbenv init -)"
+export RBENV_ROOT=${RUBY_DIR}/rbenv
+path_add ${RBENV_ROOT}/bin
+is_exe rbenv && eval "$(rbenv init -)"
+
+alias bex='bundle exec'
+
+#ruby-build installs a non-Homebrew OpenSSL for each Ruby version installed and these are never upgraded.
+#
+#To link Rubies to Homebrew's OpenSSL 1.1 (which is upgraded) add the following
+#to your /Users/mjl/.bash_profile:
+
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+
+#Note: this may interfere with building old versions of Ruby (e.g <2.4) that use
+#OpenSSL <1.1.
